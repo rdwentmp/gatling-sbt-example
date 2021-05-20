48aa824435d9d966b45b72d9c78a64a4553eff68
rdwentmp <maaarek7823@gmail.com>
2021-05-20 15:12:50 +0200

    Test: ws simulation example

---
+++ src/test/scala/computerdatabase/WebSocketExample.scala
@@ -0,0 +1,21 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef.{http, _}
+
+class WebSocketExample extends Simulation {
+
+  val httpProtocol = http.baseUrl("http://demo.kazzing.com").wsBaseUrl("ws://demos.kaazing.com")
+
+  val scn = scenario("testWebSocketScenario")
+        .exec(http("first request").get("/")).pause(2)
+        exec(ws("Connect WS").connect("/echo.k1=Y")
+        .onConnected(
+          exec(ws("Perform auth")
+            .sendText("Some auth token")
+          .await("20")(ws.checkTextMessage("check name").check(regex(".*auth.*")))
+        )))
+        .exec(ws("Close conection").close)
+
+    setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
